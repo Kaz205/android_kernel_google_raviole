16d72a1d230645cb663f0114466dc3a10f9da717
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 12:41:56 -0800

    selinux: Remove audit dependency
    Auditing comes with a lot of overhead due to string assembly via
    vsnprintf. It isn't actually needed to make SELinux work, so remove
    SELinux's artificial dependency on it to make it possible to use SELinux
    without the unneeded overhead.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/audit.h
+++ include/linux/audit.h
@@ -260,6 +260,10 @@ static inline unsigned int audit_get_sessionid(struct task_struct *tsk)
 {
 	return AUDIT_SID_UNSET;
 }
+static inline int audit_update_lsm_rules(void)
+{
+	return 0;
+}
 
 #define audit_enabled AUDIT_OFF
 
