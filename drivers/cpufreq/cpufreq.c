celtare21/kernel_google_raviole

drivers: cpufreq: Increase min freqs
Signed-off-by: celtare21 <celtare21@gmail.com>

drivers/cpufreq/cpufreq.c
@@ -2474,6 +2474,13 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	new_data.min = freq_qos_read_value(&policy->constraints, FREQ_QOS_MIN);
 	new_data.max = freq_qos_read_value(&policy->constraints, FREQ_QOS_MAX);
 
+        if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+                cpufreq_verify_within_limits(&new_data, 1197000, new_data.max);
+        if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+                cpufreq_verify_within_limits(&new_data, 1491000, new_data.max);
+        if (cpumask_test_cpu(policy->cpu, cpu_perfp_mask))
+                cpufreq_verify_within_limits(&new_data, 1826000, new_data.max);
+
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n",
 		 new_data.cpu, new_data.min, new_data.max);
 

Kaz205/android_kernel_google_raviole

Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

drivers/cpufreq/cpufreq.c
@@ -2493,6 +2493,13 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	new_data.min = freq_qos_read_value(&policy->constraints, FREQ_QOS_MIN);
 	new_data.max = freq_qos_read_value(&policy->constraints, FREQ_QOS_MAX);
 
+        if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+                cpufreq_verify_within_limits(&new_data, 1197000, new_data.max);
+        if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+                cpufreq_verify_within_limits(&new_data, 1491000, new_data.max);
+        if (cpumask_test_cpu(policy->cpu, cpu_prime_mask))
+                cpufreq_verify_within_limits(&new_data, 1826000, new_data.max);
+
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n",
 		 new_data.cpu, new_data.min, new_data.max);
 